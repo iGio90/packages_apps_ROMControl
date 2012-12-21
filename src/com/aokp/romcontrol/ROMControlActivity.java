aa6ce87c3c2ed8a4fd12e30e8787e145202fdebc
iGio90 <giovanni.rocca.90@gmail.com>
2012-12-21 09:28:17 +0100

    Revert "Spen Options 2/2: Enable Spen options"
    This reverts commit 1af82f3b6d9ce50f5072c31c735627201b944cf0.

--- src/com/aokp/romcontrol/ROMControlActivity.java
+++ src/com/aokp/romcontrol/ROMControlActivity.java
@@ -41,7 +41,6 @@ public class ROMControlActivity extends PreferenceActivity implements ButtonBarH
     private static final String TAG = "ROM_Control";
 
     private static boolean hasNotificationLed;
-    private static boolean hasSPen;
     private static String KEY_USE_ENGLISH_LOCALE = "use_english_locale";
 
     protected HashMap<Integer, Integer> mHeaderIndexMap = new HashMap<Integer, Integer>();
@@ -65,7 +64,6 @@ public class ROMControlActivity extends PreferenceActivity implements ButtonBarH
         mTablet = Settings.System
                 .getBoolean(getContentResolver(), Settings.System.TABLET_UI, false);
         hasNotificationLed = getResources().getBoolean(R.bool.has_notification_led);
-        hasSPen = getResources().getBoolean(R.bool.config_stylusGestures);
         mVibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         defaultLocale = Locale.getDefault();
         Log.i(TAG, "defualt locale: " + defaultLocale.getDisplayName());
@@ -212,10 +210,6 @@ public class ROMControlActivity extends PreferenceActivity implements ButtonBarH
                 }
             } else if (header.id == R.id.vibrations) {
                 if (mVibrator == null || !mVibrator.hasVibrator()) {
-                    target.remove(i);
-                }
-            } else if (header.id == R.id.spen) {
-                if (!hasSPen) {
                     target.remove(i);
                 }
             }
