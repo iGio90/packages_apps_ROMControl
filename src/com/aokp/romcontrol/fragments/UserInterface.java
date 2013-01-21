f1875e30c2573d6dba7c83a3129a73e8ad554102
Roman Birg <romanbirg@gmail.com>
2013-01-20 21:36:27 -0600

    new bootanimation (2/2)
    Change-Id: I682e8e050ccaf9c7182f9a1689db1db9e6fe904a
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

--- src/com/aokp/romcontrol/fragments/UserInterface.java
+++ src/com/aokp/romcontrol/fragments/UserInterface.java
@@ -43,6 +43,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.aokp.romcontrol.AOKPPreferenceFragment;
 import com.aokp.romcontrol.R;
+import com.aokp.romcontrol.service.CodeReceiver;
 import com.aokp.romcontrol.util.AbstractAsyncSuCMDProcessor;
 import com.aokp.romcontrol.util.CMDProcessor;
 import com.aokp.romcontrol.util.Helpers;
@@ -204,6 +205,20 @@ public class UserInterface extends AOKPPreferenceFragment {
         }
         mCustomBootAnimation.setEnabled(!mDisableBootAnimation.isChecked());
         return bootAnimationExists;
+    }
+
+    private void resetSwaggedOutBootAnimation() {
+        if(new File("/data/local/bootanimation.user").exists()) {
+            // we're using the alt boot animation
+            new AsyncTask<Void, Void, Void>() {
+                @Override
+                protected Void doInBackground(Void... params) {
+                    new CMDProcessor().su.run("mv /data/local/bootanimation.user /data/local/bootanimation.zip");
+                    return null;
+                }
+            }.execute();
+        }
+        CodeReceiver.setSwagInitiatedPref(mContext, false);
     }
 
     private void updateCustomLabelTextSummary() {
@@ -430,6 +445,7 @@ public class UserInterface extends AOKPPreferenceFragment {
     }
 
     private void openBootAnimationDialog() {
+        resetSwaggedOutBootAnimation();
         Log.e(TAG, "boot animation path: " + mBootAnimationPath);
         if(mCustomBootAnimationDialog != null) {
             mCustomBootAnimationDialog.cancel();
@@ -731,6 +747,7 @@ public class UserInterface extends AOKPPreferenceFragment {
     }
 
     private void DisableBootAnimation() {
+        resetSwaggedOutBootAnimation();
         CMDProcessor term = new CMDProcessor();
         if (!term.su.runWaitFor(
                 "grep -q \"debug.sf.nobootanimation\" /system/build.prop")
