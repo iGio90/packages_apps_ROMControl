737b59386a78175a4e47665d4fd915851e6a8561
szl.kiev <szl.kiev@gmail.com>
2013-02-16 12:12:54 +0700

    Remove "Enable Volume Panel"
    Change-Id: I8a4c46684619cc5c0d4b26cfa3f065f02a3dc63b

--- src/com/aokp/romcontrol/fragments/Sound.java
+++ src/com/aokp/romcontrol/fragments/Sound.java
@@ -24,7 +24,6 @@ import com.aokp.romcontrol.service.HeadphoneService;
 public class Sound extends AOKPPreferenceFragment
         implements OnPreferenceChangeListener {
 
-    private static final String PREF_ENABLE_VOLUME_OPTIONS = "enable_volume_options";
     private static final String PREF_HEADPHONES_PLUGGED_ACTION = "headphone_audio_mode";
     private static final String PREF_BT_CONNECTED_ACTION = "bt_audio_mode";
     private static final String PREF_FLIP_ACTION = "flip_mode";
@@ -34,7 +33,6 @@ public class Sound extends AOKPPreferenceFragment
     private static final String PREF_LESS_NOTIFICATION_SOUNDS = "less_notification_sounds";
 
     SharedPreferences prefs;
-    CheckBoxPreference mEnableVolumeOptions;
     ListPreference mHeadphonesPluggedAction;
     ListPreference mBTPluggedAction;
     ListPreference mFlipAction;
@@ -50,11 +48,6 @@ public class Sound extends AOKPPreferenceFragment
         addPreferencesFromResource(R.xml.prefs_sound);
         PreferenceManager.setDefaultValues(mContext, R.xml.prefs_sound, true);
         prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
-
-        mEnableVolumeOptions = (CheckBoxPreference) findPreference(PREF_ENABLE_VOLUME_OPTIONS);
-        mEnableVolumeOptions.setChecked(Settings.System.getBoolean(getActivity()
-                .getContentResolver(),
-                Settings.System.ENABLE_VOLUME_OPTIONS, false));
 
         mAnnoyingNotifications = (ListPreference) findPreference(PREF_LESS_NOTIFICATION_SOUNDS);
         mAnnoyingNotifications.setOnPreferenceChangeListener(this);
@@ -86,14 +79,7 @@ public class Sound extends AOKPPreferenceFragment
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
             Preference preference) {
-        if (preference == mEnableVolumeOptions) {
 
-            boolean checked = ((CheckBoxPreference) preference).isChecked();
-            Settings.System.putBoolean(getActivity().getContentResolver(),
-                    Settings.System.ENABLE_VOLUME_OPTIONS, checked);
-            return true;
-
-        }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
